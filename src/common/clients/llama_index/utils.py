d05422d77e1f00ea2560bf8896c12191c72fb476
Kristin Lindquist <lindquist.kristin@gmail.com>
2023-05-20 17:46:21 -0600

    creating dir

--- src/common/clients/llama_index/utils.py
+++ src/common/clients/llama_index/utils.py
@@ -1,5 +1,9 @@
+import pathlib
+
 from common.clients.llama_index.constants import BASE_STORAGE_DIR
 
 
 def get_persist_dir(namespace: str) -> str:
-    return f"{BASE_STORAGE_DIR}/{namespace}"
+    directory = f"{BASE_STORAGE_DIR}/{namespace}"
+    pathlib.Path(directory).mkdir(parents=True, exist_ok=True)
+    return directory
