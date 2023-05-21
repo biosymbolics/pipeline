a77e5b610ca3d04be2f52aa9fee084efb003b6f8
Kristin Lindquist <lindquist.kristin@gmail.com>
2023-05-21 10:53:02 -0600

    try/catch

--- src/workflows/build.py
+++ src/workflows/build.py
@@ -3,17 +3,44 @@ Workflows for building up data
 """
 
 from datetime import datetime
+import logging
 
 from sources.sec.knowledge_graph import build_knowledge_graph
+
+PHARMA_TICKERS = [
+    "PFE",
+    "JNJ",
+    "NVS",
+    "APPV",
+    "AMGN",
+    "GSK",
+    "GILD",
+    "NVO",
+    "TAK",
+    "LLY",
+    "AZN",
+    "BAYRY",
+    "RHHBY",
+    "MTZPY",
+    "MRK",
+    "BMY",
+]
+
+
+def build_sec():
+    start_date = datetime(2020, 1, 1)
+    for ticker in PHARMA_TICKERS:
+        try:
+            build_knowledge_graph(ticker, start_date)
+        except Exception as ex:
+            logging.error("Error encountered building %s: %s", ticker, ex)
 
 
 def main():
     """
     Main
     """
-    # PFE, JNJ, NVS (Novartis), RHHBY (Roche), APPV, MRK, Bristol Myers Squibb (BMY)
-    start_date = datetime(2020, 1, 1)
-    build_knowledge_graph("MRK", start_date)
+    build_sec()
 
 
 if __name__ == "__main__":
