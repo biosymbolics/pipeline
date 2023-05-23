6ab37749ef72660239758d706be9f9fc169e8c3a
Kristin Lindquist <lindquist.kristin@gmail.com>
2023-05-23 16:11:53 -0600

    linked kb entities

--- src/common/ner/spacy.py
+++ src/common/ner/spacy.py
@@ -3,23 +3,47 @@ Named-entity recognition using spacy
 """
 import spacy
 from spacy.tokens import Span
+from spacy.language import Language
 from pydash import flatten
 
 from common.ner.types import is_sci_spacy_linker
+from .types import KbLinker
 
 ENTITY_TYPES = ["PRODUCT"]
 
-ATTRIBUTES = ["text", "label_", "kb_id_"]
+SUMMARY_ATTRIBUTES = ["text", "label_", "kb_id_"]
 
 
 def __summarize(entities: list[Span]):
     for ent in entities:
-        for attr in ATTRIBUTES:
+        for attr in SUMMARY_ATTRIBUTES:
             try:
                 if hasattr(ent, attr):
                     print(f"Entity {attr} : {getattr(ent, attr)}")
             except Exception as ex:
                 print("tried %s", ex)
+
+
+def __get_kb_linker(nlp: Language) -> KbLinker:
+    """
+    Get the KB linker from the nlp pipeline
+    """
+    linker = nlp.get_pipe("scispacy_linker")
+
+    if not is_sci_spacy_linker(linker):
+        raise Exception("Invalid linker")
+
+    kb_linker = linker.get("kb")
+    return kb_linker
+
+
+def get_kb_entities(entities: list[Span], kb_linker: KbLinker) -> list[Span]:
+    """
+    Get the KB entities from the entities
+    """
+    kb_ents = flatten([entity._.kb_ents for entity in entities])
+    linked_ents = [kb_linker.cui_to_entity[umls_ent[0]] for umls_ent in kb_ents]  # type: ignore
+    return linked_ents
 
 
 def extract_named_entities(content: list[str]) -> list[str]:
@@ -35,17 +59,12 @@ def extract_named_entities(content: list[str]) -> list[str]:
     )
     entities = flatten([nlp(batch).ents for batch in content])
 
-    linker = nlp.get_pipe("scispacy_linker")
-
-    if not is_sci_spacy_linker(linker):
-        raise Exception("Invalid linker")
-
-    kb_linker = linker.get("kb")
-    kb_ents = flatten([entity._.kb_ents for entity in entities])
-    for umls_ent in kb_ents:
-        print(kb_linker.cui_to_entity[umls_ent[0]])  # type: ignore
+    linker = __get_kb_linker(nlp)
+    kb_entities = get_kb_entities(entities, linker)
 
     __summarize(entities)
+    __summarize(kb_entities)
+
     entity_strings = [
         entity.text if entity.label_ in ENTITY_TYPES else "" for entity in entities
     ]
--- src/common/ner/types.py
+++ src/common/ner/types.py
@@ -1,10 +1,11 @@
 from typing import Any, Callable, TypeGuard, TypedDict
 
 from spacy.pipeline import Pipe
+from spacy.tokens import Span
 
 
 NerResult = TypedDict("NerResult", {"word": str, "score": float, "entity_group": str})
-KbLinker = TypedDict("KbLinker", {"cui_to_entity": Callable})
+KbLinker = TypedDict("KbLinker", {"cui_to_entity": Callable[[Span], list[Span]]})
 SciSpacyLinker = TypedDict("SciSpacyLinker", {"kb": KbLinker})
 
 
