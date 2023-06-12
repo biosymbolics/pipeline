02970f7fc417939327963cbaab1120436c471ab2
Kristin Lindquist <lindquist.kristin@gmail.com>
2023-06-12 13:44:44 -0600

    cleanup

--- src/clients/__init__.py
+++ src/clients/__init__.py
@@ -7,12 +7,15 @@ from .low_level.big_query import (
     select_from_bg,
 )
 from .openai.gpt_client import GptApiClient
+from .patents import patent_client
 
 __all__ = [
     "chembl_client",
     "execute_bg_query",
+    "select_from_bg",
     "fetch_yfinance_data",
     "GptApiClient",
+    "patent_client",
     "query_to_bg_table",
     "BQ_DATASET_ID",
 ]
