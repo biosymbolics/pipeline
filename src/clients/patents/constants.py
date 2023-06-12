47d34fd5b2b28dd15e0801843368dc91b97bf124
Kristin Lindquist <lindquist.kristin@gmail.com>
2023-06-12 15:16:23 -0600

    CoM and MoU filtering on patents

--- src/clients/patents/constants.py
+++ src/clients/patents/constants.py
@@ -1,3 +1,28 @@
+# less likely
+# "C09",  # dyes; paints; polishes; natural resins; adhesives; compositions not otherwise provided for
+# "C10",  # petroleum, gas or coke industries; technical gases containing carbon monoxide; fuels; lubricants; peat
+# "C11",  # ANIMAL OR VEGETABLE OILS, FATS, FATTY SUBSTANCES OR WAXES; FATTY ACIDS THEREFROM; DETERGENTS; CANDLES
+COMPOSITION_OF_MATTER_IPC_CODES = [
+    "C01",  # inorganic chemistry
+    "C07",  # organic chemistry
+    "C08",  # organic macromolecular compounds
+    "C12",  # biochemistry; beer; spirits; wine; vinegar; microbiology; enzymology; mutation or genetic engineering
+]
+
+METHOD_OF_USE_IPC_CODES = [
+    "A01N",  # Methods of using biocides or herbicides
+    "A61B",  # Methods of using surgical instruments or devices
+    "A61F",  # Methods of using implants or prostheses
+    "A61K",  # Preparations for medical, dental, or toilet purposes.
+    "A61M",  # Methods of using catheters or other medical devices
+    "A61P",  # Specific therapeutic activity of chemical compounds or medicinal preparations.
+    "C07K",  # Methods of using immunoglobulins
+    "C12N",  # Methods of using microorganisms or enzymes in food or beverage production
+    "G01N",  # Methods of using immunoassays or other analytical techniques
+    "H04N",  # Methods of using video or multimedia systems
+    "H04R",  # Methods of using audio or acoustic systems
+]
+
 ADHD = "Attention Deficit Hyperactivity Disorder"
 ARTICULATION_DISORDER = "Articulation Disorder"
 ANAESTHESIA = "Anaesthesia"
--- src/clients/patents/patent_client.py
+++ src/clients/patents/patent_client.py
@@ -5,6 +5,7 @@ from typing import cast
 
 from clients import select_from_bg
 
+from .constants import COMPOSITION_OF_MATTER_IPC_CODES, METHOD_OF_USE_IPC_CODES
 from .utils import get_max_priority_date
 from .types import PatentApplication
 
@@ -16,7 +17,7 @@ SEARCH_RETURN_FIELDS = [
     "assignees",
     # "cited_by",
     "country",
-    "cpc_codes",
+    # "cpc_codes",
     # "embedding_v1 as embeddings",
     "inventors",
     "ipc_codes",
@@ -27,11 +28,31 @@ SEARCH_RETURN_FIELDS = [
     "url",
 ]
 
+# composition of matter filter
+COM_FILTER = (
+    "("
+    "SELECT COUNT(1) FROM UNNEST(ipc_codes) AS ipc "
+    f"JOIN UNNEST({COMPOSITION_OF_MATTER_IPC_CODES}) AS com_code "  # composition of matter
+    "ON starts_with(ipc, com_code)"
+    ") > 0"
+)
+
+# method of use filter
+MOU_FILTER = (
+    "("
+    "SELECT COUNT(1) FROM UNNEST(ipc_codes) AS ipc "
+    f"JOIN UNNEST({METHOD_OF_USE_IPC_CODES}) AS mou_code "  # method of use
+    "ON starts_with(ipc, mou_code)"
+    ") = 0"
+)
+
 
 def search_patents(terms: list[str]) -> list[PatentApplication]:
     """
     Search patents by terms
-    Filters on priority date
+    Filters on
+    - priority date
+    - at least one composition of matter, no method of use (TODO: too stringent?)
 
     Args:
         terms (list[str]): list of terms to search for
@@ -39,21 +60,27 @@ def search_patents(terms: list[str]) -> list[PatentApplication]:
     Returns: a list of matching patent applications
 
     TODO: doesn't actually match typing - in particular, need to aggregate annotations.
+
+    Example:
+        >>> search_patents(['asthma', 'astrocytoma'])
     """
     max_priority_date = get_max_priority_date(10)
+    fields = ",".join(SEARCH_RETURN_FIELDS)
     query = (
         "WITH filtered_entities AS ( "
         "SELECT * "
         "FROM patents.entities, UNNEST(annotations) as annotation "
         f"WHERE annotation.term IN UNNEST({terms}) "
         ") "
-        "SELECT "
-        f"{','.join(SEARCH_RETURN_FIELDS)} "
+        f"SELECT {fields} "
         "FROM patents.applications AS applications "
         "JOIN filtered_entities AS entities "
         "ON applications.publication_number = entities.publication_number "
-        f"WHERE priority_date > {max_priority_date} "
-        "limit 100"
+        "WHERE "
+        f"priority_date > {max_priority_date} "  # min patent life
+        f"AND {COM_FILTER} "
+        f"AND {MOU_FILTER} "
+        "limit 1000"
     )
     results = select_from_bg(query)
     return cast(list[PatentApplication], results)
