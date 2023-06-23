1eb56c7b09786a315e7fff25538aced1c79de2ed
Kristin Lindquist <lindquist.kristin@gmail.com>
2023-06-23 15:51:50 -0600

    table extract fix

--- src/common/utils/extraction/pdf.py
+++ src/common/utils/extraction/pdf.py
@@ -9,7 +9,7 @@ import polars as pl
 import pandas as pd
 
 
-def get_tables(filename: str = "./PFE-2021.pdf"):
+def get_tables(filename: str = "./PFE-2021.pdf") -> list[pl.DataFrame]:
     """
     Extract tables from PDF and return as dataframes
 
@@ -22,15 +22,14 @@ def get_tables(filename: str = "./PFE-2021.pdf"):
         elements = partition(filename=filename, pdf_infer_table_structure=True)
         elem_dict = convert_to_dict(elements)
         tables = [
-            pl.from_dataframe(pd.read_html(elem["metadata"]["text_as_html"])[0])
+            pd.read_html(elem["metadata"]["text_as_html"])[0]
             for elem in elem_dict
             if elem["type"] == "Table"
             and elem["metadata"].get("text_as_html") is not None
         ]
     except Exception as e:
         logging.error("Error extracting tables: %s", e)
-        traceback.print_exc()
-        return None
+        raise e
     for table in tables:
         table.head()
-    return tables
+    return [pl.from_pandas(table) for table in tables]
