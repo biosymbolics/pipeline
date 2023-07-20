e0dba917fab886916d88c574c9d146daed441d5f
Kristin Lindquist <lindquist.kristin@gmail.com>
2023-07-20 13:40:18 -0600

    more cleanup tweaks

--- src/scripts/patents/clean_after_ner.py
+++ src/scripts/patents/clean_after_ner.py
@@ -2,7 +2,15 @@
 To run after NER is complete
 """
 import sys
+import logging
+
+from system import initialize
+
+initialize()
+
 from clients.low_level.big_query import execute_bg_query
+
+TABLE = "fair-abbey-386416.patents.biosym_annotations"
 
 
 def fix_of_for_annotations():
@@ -12,42 +20,128 @@ def fix_of_for_annotations():
     """
     # Define terms
     terms = [
-        "fragments",
-        "modulators",
-        "inhibitors",
-        "inhibition",
         "blockers",
-        "antagonists",
-        "agonists",
-        "ligands",
+        "blockade",
+        "blocking",
         "reagents",
         "derivatives",
         "antigens",
+        "neoantigens",
         "compositions",
-        "activators",
+        "compounds",
+        "formulations",
         "linkers",
         "isoforms",
-        "conjugates",
         "stereoisomers",
-        "polypeptides",
-        "surfactant",
+        "surfactants",
         "antibodies",
         "antibody",
-        "stimulator",
+        "antibody molecules",
+        "autoantibody",
+        "autoantibodies",
+        "donors",
+        "prodrugs",
+        "pro-drugs",
+        "adjuvants",
+        "vaccines",
+        "vaccine adjuvants",
+        "analogs",
+        "analogues",
+        "enzymes",
+        "anions",
+        "ions",
+        "drugs",
     ]
 
-    # Loop over terms
-    for term in terms:
-        # Construct the SQL statement
+    term_sets = [
+        ["modulators", "modulating", "modulations"],
+        ["modifiers", "modifying", "modifications"],
+        ["inhibitors", "inhibition", "inhibiting", "inhibits"],
+        ["agonists", "agonizing", "agonizes", "agonism"],
+        ["antagonists", "antagonizing", "antagonizes", "antagonism"],
+        [
+            "activators",
+            "activations",
+            "activating",
+        ],
+        ["neuromodulations", "neuromodulators", "neuromodulating", "neuromodulates"],
+        ["simulators", "simulations", "simulating", "simulates"],
+        ["conjugations", "conjugating", "conjugates"],
+        ["modulates", "modulates? binding"],
+        ["(?:poly)peptides", "(?:poly)peptides? binding"],
+        ["proteins", "proteins? binding"],
+        ["(?:poly)?nucleotides", "(?:poly)?nucleotides? binding"],
+        ["molecules", "molecules? binding"],
+        ["ligands", "ligands? binding", "ligands? that bind"],
+        ["fragments", "fragments? binding", "fragments? that bind"],
+        ["promotion", "promoting", "promotes"],
+        ["enhancement", "enhancing", "enhances", "enhancer"],
+    ]
+
+    prefixes = [
+        "cyclic",
+        "reversible",
+        "irreversible",
+        "new",
+        "bispecific",
+        "bi-specific",
+        "monoclonal",
+        "acceptable",
+        "receptor",
+        "encoding",
+        "encoders",
+        "positive allosteric",
+        "small molecule",
+        "potent",
+        "inventive",
+        "selective",
+        "novel",
+        "heterocyclic",
+        "partial",
+        "inverse",
+        "dual",
+        "single",
+    ]
+
+    prefix_re = "|".join([p + " " for p in prefixes])
+
+    def get_query(term, second_term=None):
+        if not second_term:
+            second_term = term
         sql = f"""
-        UPDATE `fair-abbey-386416.patents.biosym_annotations` a
-        SET original_term=(REGEXP_EXTRACT(title, '(?i)((?:new |bispecific |monoclonal |acceptable |receptor |positive allosteric |small molecule |potent |inventive |selective |novel |heterocyclic |partial |inverse )?{term}? (?:of|for) (?:(?:the|a)\\s)?.*?)(?:and|useful|for|,|$)'))
-        FROM `fair-abbey-386416.patents.gpr_publications` p
-        WHERE p.publication_number=a.publication_number
-        AND REGEXP_CONTAINS(original_term, "^(?i)(?:new |bispecific |monoclonal |acceptable |receptor |positive allosteric |small molecule |potent |inventive |selective |novel |heterocyclic |partial |inverse )?{term}?$")
-        AND REGEXP_CONTAINS(p.title, '(?i).*{term}? (?:of|for).*')
+            UPDATE {TABLE} a
+            SET original_term=(REGEXP_EXTRACT(title, '(?i)((?:{prefix_re})*{term}? (?:of|for|the|that|to) (?:(?:the|a) )?.*?)(?:and|useful|for|,|$)'))
+            FROM `fair-abbey-386416.patents.gpr_publications` p
+            WHERE p.publication_number=a.publication_number
+            AND REGEXP_CONTAINS(original_term, "^(?i)(?:{prefix_re})*{second_term}?$")
+            AND REGEXP_CONTAINS(p.title, '(?i).*{term}? (?:of|for).*')
         """
+        return sql
+
+    def get_hyphen_query(term):
+        sql = f"""
+            UPDATE {TABLE} a
+            SET original_term=(REGEXP_EXTRACT(title, '(?i)([A-Za-z0-9]+-{term}?)'))
+            FROM `fair-abbey-386416.patents.gpr_publications` p
+            WHERE p.publication_number=a.publication_number
+            AND REGEXP_CONTAINS(original_term, '^(?i){term}?$')
+            AND REGEXP_CONTAINS(p.title, '(?i).*[A-Za-z0-9]+-{term}?.*')
+        """
+        return sql
+
+    for term in terms:
+        sql = get_query(term)
         execute_bg_query(sql)
+
+    # for term in [*terms, *[t for term_set in term_sets for t in term_set]]:
+    #     sql = get_hyphen_query(term)
+    #     execute_bg_query(sql)
+
+    # loop over term sets, in which the original_term may be in another form than the title variant
+    for term_set in term_sets:
+        for term in term_set:
+            sql = get_query(term, term_set[0])
+            execute_bg_query(sql)
 
 
 def clean_annotations():
@@ -55,13 +149,20 @@ def clean_annotations():
     Remove trailing junk and silly matches
     """
     queries = [
-        "update `fair-abbey-386416.patents.biosym_annotations` set original_term=(REGEXP_REPLACE(original_term, 'such', '')) where original_term like '% such';",
-        "update  `fair-abbey-386416.patents.biosym_annotations` set original_term=(REGEXP_REPLACE(original_term, '[)]', '')) where original_term like '%)' and original_term not like '%(%';",
-        "update `fair-abbey-386416.patents.biosym_annotations` set original_term=(REGEXP_REPLACE(original_term, 'thereof', '')) where original_term like '% thereof';",
-        "delete from `fair-abbey-386416.patents.biosym_annotations` where original_term='COMPOSITION' or original_term='therapeutical' or original_term='prognosticating' or original_term in ('unsubstituted', 'medicine Compounds of formula', 'THERAPY') or original_term like '% administration' or original_term like '% patients' or original_term like 'treat %'",
+        f"update `{TABLE}` set original_term=(REGEXP_REPLACE(original_term, ' such', '')) where original_term like '% such';",
+        f"update `{TABLE}` set original_term=(REGEXP_REPLACE(original_term, 'such ', '')) where original_term like 'such %';",
+        f"update `{TABLE}` set original_term=(REGEXP_REPLACE(original_term, 'the ', '')) where original_term like 'the %';",
+        f"update `{TABLE}` set original_term=(REGEXP_REPLACE(original_term, 'excellent', '')) where original_term like '%excellent%';",
+        f"update `{TABLE}` set original_term=(REGEXP_REPLACE(original_term, 'particular ', '')) where original_term like 'particular %';",
+        f"update `{TABLE}` set original_term=(REGEXP_REPLACE(original_term, ' useful', '')) where original_term like '% useful';",
+        f"update  `{TABLE}` set original_term=(REGEXP_REPLACE(original_term, '[)]', '')) where original_term like '%)' and original_term not like '%(%';",
+        f"update `{TABLE}` set original_term=(REGEXP_REPLACE(original_term, 'thereof', '')) where original_term like '% thereof';",
+        f"delete from `{TABLE}` where original_term='COMPOSITION' or original_term='therapeutical' or original_term='prognosticating' or original_term in ('wherein said compound', 'fragment of', 'therapeutically', 'general formula (I)', 'medicine Compounds', 'liver', 'treatment regimens', 'unsubstituted', 'Compound I', 'medicinal compositions', 'COMPOUND', 'DISEASE', 'medicine Compounds of formula', 'THERAPY') or original_term like '% administration' or original_term like '% patients' or original_term like 'treat %' or original_term like 'treating %' or original_term like 'field of %'",
+        f"delete from `{TABLE}` where original_term is null",
+        f"update `{TABLE}` set domain='mechanisms' where original_term in ('tumor infiltrating lymphocytes')",
     ]
     for sql in queries:
-        execute_bg_query(sql)
+        results = execute_bg_query(sql)
 
 
 if __name__ == "__main__":
