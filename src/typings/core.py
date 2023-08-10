4e3656146e6bd08f7bbc2793b9824a0564854b2b
Kristin Lindquist <lindquist.kristin@gmail.com>
2023-08-10 07:58:19 -0600

    refactor layout

--- src/typings/core.py
+++ src/typings/core.py
@@ -1,15 +1,10 @@
 from typing import Any, TypeGuard, Union, List, Dict
-from spacy.tokens.doc import Doc as SpacyDoc
 
 JsonSerializable = Union[
     Dict[str, "JsonSerializable"], List["JsonSerializable"], str, int, float, bool, None
 ]
 
 Primitive = bool | str | int | float | None
-
-
-class Doc(SpacyDoc):
-    pass
 
 
 def is_string_list(obj: Any) -> TypeGuard[list[str]]:
@@ -23,16 +18,3 @@ def is_string_list(obj: Any) -> TypeGuard[list[str]]:
         bool: True if the object is a list of strings
     """
     return isinstance(obj, list) and all(isinstance(x, str) for x in obj)
-
-
-def is_doc_list(obj: Any) -> TypeGuard[list[Doc]]:
-    """
-    Checks if an object is a list of SpaCy docs
-
-    Args:
-        obj (Any): object to check
-
-    Returns:
-        bool: True if the object is a list of SpaCy docs
-    """
-    return isinstance(obj, list) and all(isinstance(x, Doc) for x in obj)
