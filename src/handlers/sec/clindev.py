7f7066faeafa2a3642e914100cfa23dbd559e3c2
Kristin Lindquist <lindquist.kristin@gmail.com>
2023-08-23 17:10:37 -0600

    moved clindev thing to sec

---
+++ src/handlers/sec/clindev.py
@@ -0,0 +1,50 @@
+from typing import TypedDict
+import logging
+
+from clients.sec.chat import SecChatClient
+
+
+class ClinDevParams(TypedDict):
+    indication: str
+
+
+class ClinDevEvent(TypedDict):
+    queryStringParameters: ClinDevParams
+
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+
+def ask_about_clindev(event: ClinDevEvent, context):
+    """
+    Query GPT about clindev timelines
+
+    Invocation:
+    - Local: `serverless invoke local --function ask-sec --data='{"queryStringParameters": { "question": "asthma" }}'`
+    - Remote: `serverless invoke --function ask-sec --data='{"queryStringParameters": { "indication": "asthma" }}'`
+    - API: `curl https://api.biosymbolics.ai/sec/ask/clindev?indication=asthma`
+    """
+    sec_chat = SecChatClient()
+
+    params = event.get("queryStringParameters", {})
+    indication = params.get("indication")
+
+    if not indication or len(indication) < 5:
+        logger.error(
+            "Missing or malformed query params: %s",
+            params,
+        )
+        return {
+            "statusCode": 400,
+            "body": "Missing parameter(s)",
+        }
+
+    logger.info(
+        "Fetching info for indication: %s",
+        indication,
+    )
+
+    answer = sec_chat.query_clindev(indication)
+
+    return {"statusCode": 200, "body": answer}
