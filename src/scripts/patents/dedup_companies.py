03fd8aa7734ab4a5d3431fe2e5e6ecf505c7d8f1
Kristin Lindquist <lindquist.kristin@gmail.com>
2023-09-01 13:25:07 -0600

    slightly more sophisticated owner term thingy

--- src/scripts/patents/dedup_companies.py
+++ src/scripts/patents/dedup_companies.py
@@ -1,59 +1,17 @@
-import os
 import sys
-from typing import Collection
-import csv
-import dedupe
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.cluster import DBSCAN
+import polars as pl
 
 from system import initialize
 
 initialize()
 
 from clients.low_level.postgres import PsqlDatabaseClient
+from scripts.patents.utils import clean_owners
+from utils.list import dedup
 
 COMPANY_DEDUP_FILE = "company_dedup.csv"
-
-
-def dedup_interactive(
-    data_d: dict,
-    variables: Collection,
-    output_file: str = COMPANY_DEDUP_FILE,
-    training_file="company_dedup_training.json",
-    settings_file="company_dedup_settings",
-):
-    deduper = dedupe.Dedupe(variables)
-
-    if os.path.isfile(training_file):
-        with open(training_file) as tf:
-            deduper.prepare_training(data_d, tf)
-    else:
-        deduper.prepare_training(data_d)
-    dedupe.console_label(deduper)
-    deduper.train()
-
-    with open(training_file, "w") as tf:
-        deduper.write_training(tf)
-
-    with open(settings_file, "wb") as sf:
-        deduper.write_settings(sf)
-
-    clustered_dupes = deduper.partition(data_d, 0.5)
-    cluster_membership = {}
-    for cluster_id, (records, scores) in enumerate(clustered_dupes):
-        for record_id, score in zip(records, scores):
-            cluster_membership[record_id] = {
-                "Cluster ID": cluster_id,
-                "confidence_score": score,
-            }
-
-    with open(output_file, "w") as f_output:
-        fieldnames = ["id", "company_name", "Cluster ID", "confidence_score"]
-        writer = csv.DictWriter(f_output, fieldnames=fieldnames)
-        writer.writeheader()  # # TypeError: 'int' object is not subscriptable
-
-        for row in data_d.values():
-            row_id = int(row["id"])
-            row.update({"id": row_id, **cluster_membership[row_id]})
-            writer.writerow(row)
 
 
 def dedup_companies():
@@ -68,28 +26,32 @@ def dedup_companies():
             unnest(a.assignees) as assignee
         ) s
         group by lower(company_name)
-        having count(*) > 100
+        having count(*) > 10
     """
     records = PsqlDatabaseClient().select(query)
-    company_map = dict([(i, {**record, "id": i}) for i, record in enumerate(records)])
-    variables = [
-        {"field": "company_name", "type": "String"},
+    org_names = [record["company_name"] for record in records]
+    org_names_norm = dedup(clean_owners(org_names))
+    vectorizer = TfidfVectorizer(stop_words="english", strip_accents="unicode")
+    X = vectorizer.fit_transform(org_names_norm)
+    clustering = DBSCAN(eps=0.8, min_samples=2).fit(X)
+    labels = clustering.labels_
+
+    df = pl.DataFrame({"id": labels, "company_name": org_names_norm})
+    grouped_cos = (
+        df.filter(pl.col("id") > -1)
+        .groupby("id")
+        .agg(pl.col("company_name"))
+        .drop("id")
+        .to_series()
+        .to_list()
+    )
+
+    synonyms = [
+        {"term": members[0], "synonym": m}
+        for members in grouped_cos
+        for m in members[1:]
     ]
-    dedup_interactive(company_map, variables)
-
-
-def persist_synonyms(filename: str = COMPANY_DEDUP_FILE):
-    reader = csv.DictReader(open(filename))
-    records = [row for row in reader]
-
-    lookup_map = dict([(record["id"], record["company_name"]) for record in records])
-
-    synonym_map_records = [
-        {"synonym": record["company_name"], "term": lookup_map[record["id"]]}
-        for record in records
-        if record["Cluster ID"] != record["id"]
-    ]
-    records = PsqlDatabaseClient().insert_into_table(synonym_map_records, "synonym_map")
+    records = PsqlDatabaseClient().insert_into_table(synonyms, "synonym_map")
 
 
 def main():
