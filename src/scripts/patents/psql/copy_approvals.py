456d3cf16858925f3d7aef4af3e8283208acaf3d
Kristin Lindquist <lindquist.kristin@gmail.com>
2023-09-08 11:50:47 -0600

    removed junk

--- src/scripts/patents/psql/copy_approvals.py
+++ src/scripts/patents/psql/copy_approvals.py
@@ -55,47 +55,6 @@ def copy_patent_approvals():
     )
 
 
-def copy_putative_patent_approvals():
-    """
-    Copy data from Postgres (drugcentral) to Postgres (patents)
-    """
-    PATENT_FIELDS = [
-        "concat('US-', patent_no) as publication_number",
-        "(array_agg(appl_no))[1] as nda_number",
-        "(array_agg(prod.ndc_product_code))[1] as ndc_code",
-        "(array_agg(trade_name))[1] as brand_name",
-        "(ARRAY_TO_STRING(array_agg(distinct s.name), '+')) as generic_name",
-        "(array_agg(stem))[1] as stem",
-        "(array_agg(applicant))[1] as applicant",
-        "(array_agg(prod.marketing_status))[1] as application_type",
-        "(array_agg(approval_date))[1] as approval_date",
-        "(array_agg(patent_expire_date))[1] as patent_expire_date",
-        "(array_agg(pv.description))[1] as patent_indication",
-        "(array_agg(cd_formula))[1] as formula",
-        "(array_agg(smiles))[1] as smiles",
-        "(array_agg(cd_molweight))[1] as molweight",
-        "(array_agg(active_ingredient_count))[1] as active_ingredient_count",
-        "(array_agg(pv.route))[1] as route",
-    ]
-    source_sql = f"""
-        select {", ".join(PATENT_FIELDS)}
-        from ob_patent_view pv, product prod, structures s
-        where lower(pv.trade_name) = lower(prod.product_name)
-        AND s.id = pv.struct_id
-        group by pv.patent_no
-    """
-    source_db = "drugcentral"
-    dest_db = "patents"
-    dest_table_name = PATENT_APPROVALS_TABLE
-    PsqlDatabaseClient.copy_between_db(
-        source_db=source_db,
-        source_sql=source_sql,
-        dest_db=dest_db,
-        dest_table_name=dest_table_name,
-        truncate_if_exists=False,
-    )
-
-
 def main():
     """
     Copy data from Postgres (drugcentral) to Postgres (patents)
