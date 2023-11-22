23f77cb1f39e61faaa479df2b1af037fdcc1fac7
Kristin Lindquist <lindquist.kristin@gmail.com>
2023-11-22 11:23:52 -0700

    more financial metrics

--- src/typings/companies.py
+++ src/typings/companies.py
@@ -10,7 +10,12 @@ COMPANY_STR_KEYS = [
     "net_debt",
     "current_ratio",
     "debt_equity_ratio",
+    "ebitda",
+    "gross_profit",
+    "return_on_equity",
+    "return_on_research_capital",
     "is_bad_current_ratio",
+    "is_low_return_on_equity",
     "is_trading_below_cash",
     "is_bad_debt_equity_ratio",
 ]
@@ -31,7 +36,10 @@ class Company(Dataclass):
     is_trading_below_cash: bool
     is_bad_current_ratio: bool
     is_bad_debt_equity_ratio: bool
+    is_low_return_on_equity: bool
     market_cap: float | None
+    return_on_equity: float | None
+    return_on_research_capital: float | None
     net_debt: float | None
     total_debt: float | None
     parent_company_id: Optional[str]  # id
