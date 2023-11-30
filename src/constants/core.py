ca93b394601318ff9e700466e2e7f281c69e9159
Kristin Lindquist <lindquist.kristin@gmail.com>
2023-11-30 09:00:54 -0700

    mapping table for wo publication_number to all others, for improved perf in patent client

--- src/constants/core.py
+++ src/constants/core.py
@@ -13,6 +13,8 @@ PATENT_TO_REGULATORY_APPROVAL_TABLE = "patent_to_regulatory_approval"
 PATENT_TO_TRIAL_TABLE = "patent_to_trial"
 TRIALS_TABLE = "trials"
 APPLICATIONS_TABLE = "applications"
+PUBLICATION_NUMBER_MAP_TABLE = "publication_number_map"
+
 ANNOTATIONS_TABLE = "annotations"
 TERMS_TABLE = "terms"
 TERM_IDS_TABLE = "term_ids"
