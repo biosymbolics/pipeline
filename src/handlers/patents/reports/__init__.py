ea5ba6d040b4c59090ae0d6df6bcd5aff29a775b
Kristin Lindquist <lindquist.kristin@gmail.com>
2023-11-29 17:09:49 -0700

    disabled topics export

--- src/handlers/patents/reports/__init__.py
+++ src/handlers/patents/reports/__init__.py
@@ -1,12 +1,12 @@
 from .graph import graph_patent_characteristics
 from .summarize import summarize
 from .time import aggregate_over_time
-from .topic import analyze_topics
+
+# from .topic import analyze_topics
 from .x_by_y import x_by_y
 
 __all__ = [
     "aggregate_over_time",
-    "analyze_topics",
     "graph_patent_characteristics",
     "summarize",
     "x_by_y",
