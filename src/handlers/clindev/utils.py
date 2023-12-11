6ba8cbf2d7a14ca0648a54a08eb3e19b8c11237e
Kristin Lindquist <lindquist.kristin@gmail.com>
2023-12-11 10:18:05 -0700

    trial search handler

---
+++ src/handlers/clindev/utils.py
@@ -0,0 +1,26 @@
+from typings.client import RawTrialSearchParams, TrialSearchParams
+
+
+def parse_params(
+    _params: RawTrialSearchParams,
+    default_limit: int = 800,
+) -> TrialSearchParams:
+    """
+    Parse patent params
+    """
+    # combine default and provided params
+    p = RawTrialSearchParams(**_params.__dict__)
+
+    # parse ";"-delimited terms
+    terms_list = [t.strip() for t in (p.terms.split(";") if p.terms else [])]
+
+    limit = p.limit or default_limit
+
+    return TrialSearchParams(
+        **{
+            "terms": terms_list,
+            "query_type": p.query_type,
+            "limit": limit,
+            "skip_cache": p.skip_cache,
+        }
+    )
