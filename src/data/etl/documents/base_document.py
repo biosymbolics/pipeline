43f0de94a277f70c7aa1ed00666398a2bd6f136e
Kristin Lindquist <lindquist.kristin@gmail.com>
2024-02-01 14:42:44 -0700

    checksums on doc etl

--- src/data/etl/documents/base_document.py
+++ src/data/etl/documents/base_document.py
@@ -2,7 +2,10 @@
 Abstract document ETL class
 """
 from abc import abstractmethod
+import asyncio
 import logging
+from typing import Literal
+from clients.low_level.prisma import prisma_client
 
 from system import initialize
 
@@ -14,9 +17,11 @@ from data.etl.types import BiomedicalEntityLoadSpec
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
+DocumentType = Literal["patent", "regulatory_approval", "trial"]
+
 
 class BaseDocumentEtl:
-    def __init__(self, document_type: str):
+    def __init__(self, document_type: DocumentType):
         self.document_type = document_type
 
     @abstractmethod
@@ -39,3 +44,22 @@ class BaseDocumentEtl:
             logger.info("Deleting documents in order to re-create")
             await self.delete_documents()
         await self.copy_documents()
+        await self.checksum()
+
+    async def checksum(self):
+        """
+        Quick checksum
+        TODO: raise exceptions if wrong
+        """
+        client = await prisma_client(300)
+        checksums = {
+            self.document_type: "SELECT COUNT(*) FROM {self.document_type}",
+            "owners": "SELECT COUNT(*) FROM ownable WHERE {self.document_type}_id IS NOT NULL",
+            "indications": "SELECT COUNT(*) FROM indicatable WHERE {self.document_type}_id IS NOT NULL",
+            "interventions": "SELECT COUNT(*) FROM intervenable WHERE {self.document_type}_id IS NOT NULL",
+        }
+        results = await asyncio.gather(
+            *[client.query_raw(query) for query in checksums.values()]
+        )
+        for key, result in zip(checksums.keys(), results):
+            logger.info(f"Trial load checksum {key}: {result[0]}")
