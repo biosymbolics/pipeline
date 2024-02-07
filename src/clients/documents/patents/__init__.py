61cf73bb73c214c618275d8224b9b620f25777a0
Kristin Lindquist <lindquist.kristin@gmail.com>
2024-02-07 11:41:00 -0700

    moved generic client out of patents

--- src/clients/documents/patents/__init__.py
+++ src/clients/documents/patents/__init__.py
@@ -1,14 +1,9 @@
-from .autocomplete_client import (
-    autocomplete,
-)
 from .patents_client import find_many
 from .patents_search import search
-from .types import AutocompleteMode, RelevancyThreshold
+from .types import RelevancyThreshold
 
 __all__ = [
-    "autocomplete",
     "find_many",
     "search",
-    "AutocompleteMode",
     "RelevancyThreshold",
 ]
--- src/clients/documents/patents/types.py
+++ src/clients/documents/patents/types.py
@@ -2,14 +2,7 @@
 Patent types
 """
 
-from dataclasses import dataclass
 from typing import Literal, TypedDict
-
-from typings.core import Dataclass
-
-
-AutocompleteMode = Literal["id", "term"]
-AutocompleteResult = TypedDict("AutocompleteResult", {"id": str, "label": str})
 
 
 RelevancyThreshold = Literal["very low", "low", "medium", "high", "very high"]
