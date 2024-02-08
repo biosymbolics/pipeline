67cf21a148b491b778436e33e87ea1ba23187877
Kristin Lindquist <lindquist.kristin@gmail.com>
2024-02-08 05:47:12 -0700

    cleanup

--- src/data/etl/load.py
+++ src/data/etl/load.py
@@ -62,32 +62,32 @@ async def load_all(force_update: bool = False):
         force_update (bool, optional): Whether to update or merely create.
             If update, documents and their relations are first deleted.
     """
-    # # copy umls data
-    # await UmlsLoader().copy_all()
+    # copy umls data
+    await UmlsLoader().copy_all()
 
-    # # copy all biomedical entities (from all doc types)
-    # # Takes 3+ hours!!
-    # await BiomedicalEntityLoader().copy_all()
+    # copy all biomedical entities (from all doc types)
+    # Takes 3+ hours!!
+    await BiomedicalEntityLoader().copy_all()
 
-    # # copy owner data (across all documents)
-    # await OwnerLoader().copy_all()
+    # copy owner data (across all documents)
+    await OwnerLoader().copy_all()
 
-    # # copy patent data
-    # await PatentLoader(document_type="patent").copy_all(force_update)
+    # copy patent data
+    await PatentLoader(document_type="patent").copy_all(force_update)
 
-    # # copy data about approvals
-    # await RegulatoryApprovalLoader(document_type="regulatory_approval").copy_all(
-    #     force_update
-    # )
+    # copy data about approvals
+    await RegulatoryApprovalLoader(document_type="regulatory_approval").copy_all(
+        force_update
+    )
 
-    # # copy trial data
-    # await TrialLoader(document_type="trial").copy_all(force_update)
+    # copy trial data
+    await TrialLoader(document_type="trial").copy_all(force_update)
 
-    # # do final biomedical entity stuff that requires everything else be in place
-    # await BiomedicalEntityLoader().post_finalize()
+    # do final biomedical entity stuff that requires everything else be in place
+    await BiomedicalEntityLoader().post_finalize()
 
-    # # finally, link owners
-    # await OwnerLoader().post_finalize()
+    # finally, link owners
+    await OwnerLoader().post_finalize()
 
     # create some materialized views for reporting
     for query in get_entity_map_matview_query():
