98616b60a3a5bfdf6f725cce5560f78c682fd1e2
Kristin Lindquist <lindquist.kristin@gmail.com>
2024-02-17 10:35:53 -0700

    working mock chat

--- src/handlers/chat/chat.py
+++ src/handlers/chat/chat.py
@@ -1,11 +1,15 @@
+import json
 from typing import TypedDict
 import logging
 from pydantic import BaseModel
+from prisma.enums import MockChatType
 
 from clients.biosym_chat.mock_chat import MockChatClient
 from clients.low_level.boto3 import retrieve_with_cache_check
 from clients.openai.gpt_client import GptApiClient
 from handlers.utils import handle_async
+from typings.core import ResultBase
+from utils.encoding.json_encoder import DataclassJSONEncoder
 
 
 class ChatParams(BaseModel):
@@ -23,6 +27,11 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 
+class ChatResponse(ResultBase):
+    content: str
+    type: MockChatType | None = None
+
+
 async def _chat(raw_event: dict, context):
     """
     Get GPT description of terms
@@ -32,21 +41,23 @@ async def _chat(raw_event: dict, context):
     - Remote: `serverless invoke --function chat --data='{"queryStringParameters": { "prompt":"tell me about gpr84", "skip_cache": true }}'`
     - API: `curl https://api.biosymbolics.ai/chat?prompt=tell me about gpr84`
     """
-    gpt_client = GptApiClient(model="gpt-3.5-turbo")  # gpt-4 too slow
+    gpt_client = GptApiClient(model="gpt-4")  # gpt-3.5-turbo
 
-    p = ChatParams(**raw_event["queryStringParameters"])
-
-    if not p or not p.prompt:
-        logger.error(
-            "Missing or malformed query params: %s",
-            p,
-        )
-        return {"statusCode": 400, "body": "Missing parameter(s)"}
+    p = ChatParams(**(raw_event["queryStringParameters"] or {}))
 
     logger.info("Asking llm: %s", p.prompt)
 
     if p.conversation_key is not None and p.message_key is not None:
-        return MockChatClient(conversation_key=p.conversation_key).query(p.message_key)
+        response = await MockChatClient(conversation_key=p.conversation_key).query(
+            p.message_key
+        )
+        if response is not None:
+            return {
+                "statusCode": 200,
+                "body": json.dumps(
+                    ChatResponse(**response.__dict__), cls=DataclassJSONEncoder
+                ),
+            }
 
     if p.skip_cache:
         answer = gpt_client.query(p.prompt)
@@ -56,7 +67,10 @@ async def _chat(raw_event: dict, context):
             lambda: gpt_client.query(p.prompt), key=key
         )
 
-    return {"statusCode": 200, "body": answer}
+    return {
+        "statusCode": 200,
+        "body": json.dumps(ChatResponse(content=answer), cls=DataclassJSONEncoder),
+    }
 
 
 chat = handle_async(_chat)
