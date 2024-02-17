0f56e73c8dd283e3f9ab57a1ee8d493d88de4af1
Kristin Lindquist <lindquist.kristin@gmail.com>
2024-02-16 17:26:35 -0700

    mock chat json

---
+++ src/scripts/misc/load_mock_chat.py
@@ -0,0 +1,40 @@
+import asyncio
+import sys
+from prisma.models import MockChat
+from prisma.enums import MockChatType
+
+from clients.low_level.prisma import prisma_client
+from utils.file import load_json_from_file
+
+
+async def load_mock_chat():
+    """
+    Load mock chat data
+    """
+    data = load_json_from_file("mock_chat.json")
+    conversations = [
+        {
+            "conversation_id": key,
+            "message_id": message["id"],
+            "content": message["content"],
+            "type": message.get("type") or MockChatType.STANDARD,
+        }
+        for key, conversation in data.items()
+        for message in conversation["messages"]
+        if isinstance(conversation["sender"], dict)
+    ]
+    client = await prisma_client(600)
+    await MockChat.prisma(client).create_many(conversations)  # type: ignore
+
+
+if __name__ == "__main__":
+    if "-h" in sys.argv:
+        print(
+            """
+            Usage: python3 -m scripts.misc.load_mock_chat
+            UMLS etl
+        """
+        )
+        sys.exit()
+
+    asyncio.run(load_mock_chat())
