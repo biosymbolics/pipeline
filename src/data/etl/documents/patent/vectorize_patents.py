1d5af8c4f2e54b1816cb3cc8f902914474690679
Kristin Lindquist <lindquist.kristin@gmail.com>
2024-02-27 09:07:03 -0700

    only with claims loading mode

--- src/data/etl/documents/patent/vectorize_patents.py
+++ src/data/etl/documents/patent/vectorize_patents.py
@@ -29,25 +29,30 @@ class PatentVectorizer(DocumentVectorizer):
         super().__init__(
             database="patents",
             dest_table=PATENT_VECTOR_TABLE,
-            text_fields=["title", "abstract"],
+            text_fields=["title", "abstract", "claims"],
             id_field="publication_number",
             processed_docs_file=VECTORIZED_PROCESSED_DOCS_FILE,
             batch_size=1000,
         )
 
-    async def _fetch_batch(self, last_id: Optional[str] = None) -> list[dict]:
+    async def _fetch_batch(
+        self, last_id: Optional[str] = None, only_with_claims: bool = False
+    ) -> list[dict]:
         """
         Fetch a batch of documents to vectorize
 
         Args:
             last_id (Optional[str], optional): last id to paginate from. Defaults to None.
+            only_with_claims (bool, optional): only fetch documents with claims. Defaults to False.
         """
+        claims_where = "AND array_length(claims, 1) > 0" if only_with_claims else ""
         pagination_where = f"AND {self.id_field} > '{last_id}'" if last_id else ""
 
         query = f"""
-            SELECT publication_number, title, abstract
+            SELECT publication_number, title, abstract, array_to_string(claims, ' ') as claims
             FROM {APPLICATIONS_TABLE}
             WHERE 1 = 1
+            {claims_where}
             {pagination_where}
             ORDER BY {self.id_field} ASC
             limit {self.batch_size}
@@ -60,12 +65,13 @@ if __name__ == "__main__":
     if "-h" in sys.argv:
         print(
             """
-            Usage: python3 -m data.etl.documents.patent.vectorize_patents [starting_id]
+            Usage: python3 -m data.etl.documents.patent.vectorize_patents [starting_id] [--only_with_claims]
             Loads NER data for patents
             """
         )
         sys.exit()
 
+    only_with_claims = "--only_with_claims" in sys.argv
     starting_id = sys.argv[1] if len(sys.argv) > 1 else None
     vectorizer = PatentVectorizer()
-    asyncio.run(vectorizer(starting_id))
+    asyncio.run(vectorizer(starting_id, only_with_claims=only_with_claims))
