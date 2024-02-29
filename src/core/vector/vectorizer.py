60d6fdee15d2bb183c2178cc48623e3f78d544c6
Kristin Lindquist <lindquist.kristin@gmail.com>
2024-02-28 18:09:33 -0700

    memory clearing

--- src/core/vector/vectorizer.py
+++ src/core/vector/vectorizer.py
@@ -1,3 +1,4 @@
+import gc
 from sentence_transformers import SentenceTransformer
 import torch
 
@@ -13,7 +14,9 @@ class Vectorizer:
     def __init__(
         self, model: str = DEFAULT_VECTORIZATION_MODEL, device: str = DEFAULT_DEVICE
     ):
-        self.embedding_model = SentenceTransformer(model, device="mps")
+        self.model = model
+        self.device = device
+        self.embedding_model = SentenceTransformer(model, device=device)
 
     def vectorize_string(self, text: str) -> torch.Tensor:
         return self.vectorize([text])[0]
@@ -32,6 +35,12 @@ class Vectorizer:
             return tensors
 
         return [torch.tensor(t) for t in tensors]
+
+    def empty_cache(self):
+        self.embedding_model = None
+        gc.collect()
+        torch.mps.empty_cache()
+        self.embedding_model = SentenceTransformer(self.model, device=self.device)
 
     def __call__(self, text: str) -> torch.Tensor:
         return self.vectorize_string(text)
